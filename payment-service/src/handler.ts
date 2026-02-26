4d5f646c956bb6135e340a4a564a047d547a5dbc
MatheusSCristo <cristo.matheus@outlook.com>
2026-02-26 20:23:29 -0300

    feat: handle exceptions in order to send message to DLQ

--- payment-service/src/handler.ts
+++ payment-service/src/handler.ts
@@ -5,22 +5,27 @@ const snsClient = new SNSClient({});
 
 export const handler: SQSHandler = async (event: SQSEvent) => {
   for (const record of event.Records) {
-    const snsPayload = JSON.parse(record.body);
-    const order = JSON.parse(snsPayload.Message);
+    try {
+      const snsPayload = JSON.parse(record.body);
+      const order = JSON.parse(snsPayload.Message);
 
-    console.log(`[Payment Service] Processando pagamento do pedido: ${order.orderId}`);
-    console.log(`[Payment Service] Valor: R$ ${order.amount}`);
+      console.log(`[Payment Service] Processando pagamento do pedido: ${order.orderId}`);
+      console.log(`[Payment Service] Valor: R$ ${order.amount}`);
 
-    const isApproved = Math.random() > 0.2;
-    order.status = isApproved ? 'APPROVED' : 'REJECTED';
+      const isApproved = Math.random() > 0.2;
+      order.status = isApproved ? 'APPROVED' : 'REJECTED';
 
-    console.log(`[Payment Service] Status do pagamento: ${order.status}`);
+      console.log(`[Payment Service] Status do pagamento: ${order.status}`);
 
-    await snsClient.send(new PublishCommand({
-      TopicArn: process.env.SNS_PAYMENT_RESULT_TOPIC_ARN,
-      Message: JSON.stringify(order),
-    }));
+      await snsClient.send(new PublishCommand({
+        TopicArn: process.env.SNS_PAYMENT_RESULT_TOPIC_ARN,
+        Message: JSON.stringify(order),
+      }));
+      console.log(`[Payment Service] Resultado publicado no SNS`);
+    } catch (error) {
+      console.error(`[Payment Service] Erro ao processar pagamento do pedido: ${error}`);
+      throw error;
+    }
 
-    console.log(`[Payment Service] Resultado publicado no SNS`);
   }
 };
